d73505636e7392b83ebe737432f852495566e1e7
eudesmieres <eumieres@gmail.com>
2023-06-22 20:33:12 -0300

    rama

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
-import NextAuth, {getServerSession} from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "@/lib/mongodb";
 
-const adminEmails = ['bibarel99@gmail.com'];
+const adminEmails = ['eumieres@gmail.com'];
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -15,7 +15,7 @@ export const authOptions = {
   ],
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
-    session: ({session,token,user}) => {
+    session: ({ session, token, user }) => {
       if (adminEmails.includes(session?.user?.email)) {
         return session;
       } else {
@@ -27,8 +27,8 @@ export const authOptions = {
 
 export default NextAuth(authOptions);
 
-export async function isAdminRequest(req,res) {
-  const session = await getServerSession(req,res,authOptions);
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
   if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
     res.end();
