48213b5d4e0928eed8a5e5ee0765f7ed9a47561f
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-04 19:52:57 -0300

    Ordenes con valores anteriores sin verificación de valor undefined, oscar

--- pages/api/tours.js
+++ pages/api/tours.js
@@ -1,37 +1,107 @@
-import {Tour} from "@/models/Tour";
-import {mongooseConnect} from "@/lib/mongoose";
-import {isAdminRequest} from "@/pages/api/auth/[...nextauth]";
+import { Tour } from "@/models/Tour";
+import { mongooseConnect } from "@/lib/mongoose";
+import { isAdminRequest } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handle(req, res) {
-  const {method} = req;
+  const { method } = req;
   await mongooseConnect();
-  await isAdminRequest(req,res);
+  await isAdminRequest(req, res);
 
-  if (method === 'GET') {
+  if (method === "GET") {
     if (req.query?._id) {
-      res.json(await Tour.findOne({_id:req.query._id}));
+      res.json(await Tour.findOne({ _id: req.query._id }));
     } else {
       res.json(await Tour.find());
     }
   }
 
-  if (method === 'POST') {
-    const {name, subtitle, description, duration, price, reservation, reservationPrice, images, includes, requirements, review, notes, promo, promoPrice, category, properties} = req.body;
+  if (method === "POST") {
+    const {
+      name,
+      subtitle,
+      description,
+      duration,
+      price,
+      reservation,
+      reservationPrice,
+      images,
+      includes,
+      requirements,
+      review,
+      notes,
+      promo,
+      promoPrice,
+      category,
+      properties,
+    } = req.body;
     const tourDoc = await Tour.create({
-      name,subtitle, description, duration, price, reservation, reservationPrice, images, includes, requirements, review, notes, promo, promoPrice, category, properties
-    })
+      name,
+      subtitle,
+      description,
+      duration,
+      price,
+      reservation,
+      reservationPrice,
+      images,
+      includes,
+      requirements,
+      review,
+      notes,
+      promo,
+      promoPrice,
+      category,
+      properties,
+    });
     res.json(tourDoc);
   }
 
-  if (method === 'PUT') {
-    const {name, subtitle, description, duration, price, reservation, reservationPrice, images, includes, requirements, review, notes, promo, promoPrice, category, properties, _id} = req.body;
-    await Tour.updateOne({_id}, {name, subtitle, description, duration, price, reservation, reservationPrice, images, includes, requirements, review, notes, promo, promoPrice, category, properties});
+  if (method === "PUT") {
+    const {
+      name,
+      subtitle,
+      description,
+      duration,
+      price,
+      reservation,
+      reservationPrice,
+      images,
+      includes,
+      requirements,
+      review,
+      notes,
+      promo,
+      promoPrice,
+      category,
+      properties,
+      _id,
+    } = req.body;
+    await Tour.updateOne(
+      { _id },
+      {
+        name,
+        subtitle,
+        description,
+        duration,
+        price,
+        reservation,
+        reservationPrice,
+        images,
+        includes,
+        requirements,
+        review,
+        notes,
+        promo,
+        promoPrice,
+        category,
+        properties,
+      }
+    );
     res.json(true);
   }
 
-  if (method === 'DELETE') {
+  if (method === "DELETE") {
     if (req.query?.id) {
-      await Tour.deleteOne({_id:req.query?.id});
+      await Tour.deleteOne({ _id: req.query?.id });
       res.json(true);
     }
   }
