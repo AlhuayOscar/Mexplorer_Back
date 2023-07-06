47312b84717ad103ee593520dd3dd6b09e874369
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-05 23:39:11 -0300

    Loading funcional para Grafico y Login, oscar

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
+import React, { useState, useEffect } from "react";
 import Layout from "@/components/Layout";
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function Home() {
-  const {data: session} = useSession();
-  return <Layout>
-    <div className="text-blue-900 flex justify-between">
-      <h2>
-        ¡Bienvenido, <b>{session?.user?.name}</b>!
-      </h2>
-      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
-        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
-        <span className="px-2">
-          {session?.user?.name}
-        </span>
+  const { data: session, status } = useSession();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1800);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading || status === "loading") {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <img
+          src="/mex_logo.png"
+          alt="Logo de México"
+          className="mt-4 max-w-350px max-h-full animate-fadeIn"
+        />
+        <h1 className="mb-4">Cargando...</h1>
+        <ClipLoader size={65} color={"#000"} loading={true} />
       </div>
-    </div>
-  </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          ¡Bienvenido, <b>{session?.user?.name}</b>!
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="" className="w-6 h-6" />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
 }
