68a11612a4d04105840bcdeceab99b6a35bd374e
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-06 02:02:09 -0300

    Panel de Admin 45% Estilado, oscar

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,36 @@ import React, { useState, useEffect } from "react";
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
 import GridLoader from "react-spinners/GridLoader";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import axios from "axios";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function Home() {
   const { data: session, status } = useSession();
   const [isLoading, setIsLoading] = useState(true);
+  const [tourData, setTourData] = useState([]);
+  const [reservedToursCount, setReservedToursCount] = useState(0);
+  const [unreservedToursCount, setUnreservedToursCount] = useState(0);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    axios.get("/api/tours").then((response) => {
+      setTourData(response.data);
       setIsLoading(false);
-    }, 2100);
-
-    return () => clearTimeout(timer);
+    });
   }, []);
+
+  useEffect(() => {
+    if (!isLoading && tourData.length > 0) {
+      const reservedCount = tourData.filter((tour) => tour.reservation).length;
+      const unreservedCount = tourData.filter(
+        (tour) => !tour.reservation
+      ).length;
+      setReservedToursCount(reservedCount);
+      setUnreservedToursCount(unreservedCount);
+    }
+  }, [isLoading, tourData]);
 
   if (isLoading || status === "loading") {
     return (
@@ -34,6 +52,113 @@ export default function Home() {
     );
   }
 
+  const tourCountData = {
+    labels: ["Con reserva", "Sin reserva"],
+    datasets: [
+      {
+        data: [reservedToursCount, unreservedToursCount],
+        backgroundColor: [
+          "#36a2eb",
+          "#ff6384",
+          "#ffce56",
+          "#ff9f40",
+          "#4bc0c0",
+          "#8e44ad",
+          "#f39c12",
+          "#16a085",
+          "#c0392b",
+        ],
+        borderColor: [
+          "#36a2eb",
+          "#ff6384",
+          "#ffce56",
+          "#ff9f40",
+          "#4bc0c0",
+          "#8e44ad",
+          "#f39c12",
+          "#16a085",
+          "#c0392b",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const uniquePrices = [...new Set(tourData.map((tour) => tour.price))];
+
+  const tourPricesData = {
+    labels: uniquePrices.map((price) => price.toString()),
+    datasets: [
+      {
+        data: uniquePrices.map((price) => {
+          return tourData.filter((tour) => tour.price === price).length;
+        }),
+        backgroundColor: [
+          "#ffce56",
+          "#ff6384",
+          "#36a2eb",
+          "#ff9f40",
+          "#4bc0c0",
+          "#8e44ad",
+          "#f39c12",
+          "#16a085",
+          "#c0392b",
+        ],
+        borderColor: [
+          "#ffce56",
+          "#ff6384",
+          "#36a2eb",
+          "#ff9f40",
+          "#4bc0c0",
+          "#8e44ad",
+          "#f39c12",
+          "#16a085",
+          "#c0392b",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const promoData = {
+    labels: ["Con promo", "Sin promo"],
+    datasets: [
+      {
+        data: [
+          tourData.filter((tour) => tour.promo).length,
+          tourData.filter((tour) => !tour.promo).length,
+        ],
+        backgroundColor: [
+          "#36a2eb",
+          "#ff6384",
+          "#ffce56",
+          "#ff9f40",
+          "#4bc0c0",
+          "#8e44ad",
+          "#f39c12",
+          "#16a085",
+          "#c0392b",
+        ],
+        borderColor: [
+          "#36a2eb",
+          "#ff6384",
+          "#ffce56",
+          "#ff9f40",
+          "#4bc0c0",
+          "#8e44ad",
+          "#f39c12",
+          "#16a085",
+          "#c0392b",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    responsive: true,
+  };
+
   return (
     <Layout>
       <div className="text-blue-900 flex justify-between">
@@ -43,6 +168,34 @@ export default function Home() {
         <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
           <img src={session?.user?.image} alt="" className="w-6 h-6" />
           <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <div className="max-w-[400px] max-h-[400px]">
+          <h3>Cantidad de Tours</h3>
+          <Doughnut
+            data={tourCountData}
+            options={chartOptions}
+            className="pixelated-chart"
+          />
+        </div>
+
+        <div className="max-w-[400px] max-h-[400px]">
+          <h3>Precios de Tours</h3>
+          <Doughnut
+            data={tourPricesData}
+            options={chartOptions}
+            className="pixelated-chart"
+          />
+        </div>
+
+        <div className="max-w-[400px] max-h-[400px]">
+          <h3>Promociones</h3>
+          <Doughnut
+            data={promoData}
+            options={chartOptions}
+            className="pixelated-chart"
+          />
         </div>
       </div>
     </Layout>
