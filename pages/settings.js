1b385ab75ae6046dc55bc03b9f7f95e38f756437
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-08-01 04:10:10 -0300

    Primer intento Mp4 Loader, oscar

--- pages/settings.js
+++ pages/settings.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Layout from "../components/Layout.js";
 import Swal from "sweetalert2";
 import GridLoader from "react-spinners/GridLoader";
+import { S3Client } from "@aws-sdk/client-s3"; // Import the AWS S3 Client
 
 const Settings = () => {
   const [urlData, setUrlData] = useState([]);
@@ -11,7 +12,8 @@ const Settings = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [saveButtonText, setSaveButtonText] = useState("Subir cambios");
   const [loading, setLoading] = useState(true);
-
+  const [isUploading, setIsUploading] = useState(false);
+  const [lastUploadedVideos, setLastUploadedVideos] = useState([]);
   // Define the array of companies and their associated colors
   const companyColors = {
     Facebook: { backgroundColor: "#3b5998", color: "#ffffff" },
@@ -161,6 +163,69 @@ const Settings = () => {
     }
   }, [newUrl]);
 
+  const uploadVideos = async (ev) => {
+    setIsUploading(true);
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      for (const file of files) {
+        if (file.type === "video/mp4") {
+          data.append("video", file);
+        } else {
+          // You can show an error message or skip unsupported files.
+          console.log("Unsupported file format:", file.name);
+        }
+      }
+      try {
+        const res = await axios.post("/api/upload/videos", data);
+
+        // Assuming you have the necessary AWS credentials and configuration set up
+        const s3 = new S3Client({ region: "your-aws-region" });
+        const bucketName = "your-bucket-name";
+        const key = res.data.filename;
+
+        const params = {
+          Bucket: bucketName,
+          Key: key,
+          Expires: 3600,
+        };
+        const videoUrl = await s3.getSignedUrl("getObject", params);
+
+        // Update the lastUploadedVideos state with the new video
+        setLastUploadedVideos((prevVideos) => [
+          { name: newUrl.nick, url: videoUrl },
+          ...prevVideos,
+        ]);
+
+        // Update the local storage with the new video data
+        localStorage.setItem(
+          "lastUploadedVideos",
+          JSON.stringify(lastUploadedVideos)
+        );
+
+        setIsUploading(false);
+      } catch (error) {
+        console.error("Error uploading videos:", error);
+        setIsUploading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Fetch last uploaded videos data from local storage on component mount
+    const lastVideosDataFromLocalStorage =
+      localStorage.getItem("lastUploadedVideos");
+    if (lastVideosDataFromLocalStorage) {
+      setLastUploadedVideos(JSON.parse(lastVideosDataFromLocalStorage));
+    }
+    fetchSettings();
+  }, []);
+
+  const handleUploadButtonClick = () => {
+    // Trigger the file input element to select the video file
+    const fileInput = document.getElementById("videoInput");
+    fileInput.click();
+  };
   return (
     <Layout>
       <div className="p-4">
@@ -267,18 +332,60 @@ const Settings = () => {
             </div>
             <input
               type="text"
-              value={newUrl.url}
-              onChange={handleNewUrlChange}
-              placeholder="Coloca la URL aquí"
-              className="border border-gray-300 px-4 py-2 w-full rounded mt-2"
-            />
-            <input
-              type="text"
               value={newUrl.nick}
               onChange={handleNewNickChange}
               placeholder="Nombre de la URL"
               className="border border-gray-300 px-4 py-2 w-full rounded mt-2"
             />
+            <input
+              type="text"
+              value={newUrl.url}
+              onChange={handleNewUrlChange}
+              placeholder="Coloca la URL aquí"
+              className="border border-gray-300 px-4 py-2 w-full rounded mt-2"
+            />
+            {/* Contenedor para el subidor de videos mp4 */}
+            <div className="mt-4">
+              <label
+                htmlFor="videoInput"
+                className="block font-medium text-gray-700"
+              >
+                Subir video mp4
+              </label>
+              <input
+                id="videoInput"
+                type="file"
+                accept=".mp4"
+                onChange={uploadVideos}
+                multiple
+                className="mt-2"
+              />
+            </div>
+
+            {/* Listado de los últimos videos subidos */}
+            <div className="mt-4">
+              <h2 className="text-xl font-bold mb-2">
+                Últimos videos subidos:
+              </h2>
+              {lastUploadedVideos.length > 0 ? (
+                <ul className="list-disc list-inside">
+                  {lastUploadedVideos.map((video, index) => (
+                    <li key={index}>
+                      <span className="font-medium">{video.name}:</span>{" "}
+                      <a
+                        href={video.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {video.url}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No hay información en el historial local.</p>
+              )}
+            </div>
             <button
               type="button"
               onClick={handleAddNewUrl}
