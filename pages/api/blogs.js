7cbb3fedde5e447c7c42714250bd720a0661f060
Alan Winkler <alanquimeywinkler@gmail.com>
2023-08-03 17:30:47 -0300

    agregué blogs en ingles

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -9,46 +9,88 @@ export default async function handle(req, res) {
   await isAdminRequest(req, res);
 
   if (method === "GET") {
-    if (req.query?._id) {
-      res.json(await Blog.findOne({ _id: req.query._id }));
-    } else {
-      res.json(await Blog.find());
+    try {
+      if (req.query?._id) {
+        return res.json(await Blog.findOne({ _id: req.query._id }));
+      } else {
+        return res.json(await Blog.find());
+      }
+    } catch (error) {
+      return res.status(400).json({ msg: error.message });
     }
   }
 
   if (method === "POST") {
-    const { title, subtitle, description, images, date, location } = req.body;
-    const blogDoc = await Blog.create({
-      title,
-      subtitle,
-      description,
-      images,
-      date,
-      location,
-    });
-    res.json(blogDoc);
-  }
-
-  if (method === "PUT") {
-    const { title, subtitle, description, images, date, location, _id } =
-      req.body;
-    const blogDoc = await Blog.updateOne(
-      { _id },
-      {
+    try {
+      const {
         title,
+        titleEng,
         subtitle,
+        subtitleEng,
         description,
+        descriptionEng,
         images,
         date,
         location,
-      }
-    );
-    res.json(blogDoc);
+      } = req.body;
+      const blogDoc = await Blog.create({
+        title,
+        titleEng,
+        subtitle,
+        subtitleEng,
+        description,
+        descriptionEng,
+        images,
+        date,
+        location,
+      });
+      return res.status(201).json(blogDoc);
+    } catch (error) {
+      return res.status(400).json({ msg: error.message });
+    }
+  }
+
+  if (method === "PUT") {
+    try {
+      const {
+        title,
+        titleEng,
+        subtitle,
+        subtitleEng,
+        description,
+        descriptionEng,
+        images,
+        date,
+        location,
+        _id,
+      } = req.body;
+      const blogDoc = await Blog.updateOne(
+        { _id },
+        {
+          title,
+          titleEng,
+          subtitle,
+          subtitleEng,
+          description,
+          descriptionEng,
+          images,
+          date,
+          location,
+        }
+      );
+      return res.json(blogDoc);
+    } catch (error) {
+      return res.status(400).json({ msg: error.message });
+    }
   }
 
   if (method === "DELETE") {
-    const { _id } = req.query;
-    await Blog.deleteOne({ _id });
-    res.json("ok");
+    try {
+      const { _id } = req.query;
+      await Blog.deleteOne({ _id });
+      return res.json("ok");
+    } catch (error) {
+      return res.status(400).json({ msg: error.message });
+    }
   }
 }
